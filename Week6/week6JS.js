4d7c73f239ca250190afbe6cc77f1d1b15d5a43a
Trumpold <bltrumpold@labs.neit.edu>
2018-08-20 13:55:43 -0400

    ajax

---
+++ Week6/week6JS.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", doit)
+
+function doit()
+{
+    var database = document.createElement('div');
+    var webDev = document.createElement('div');
+    var softDev = document.createElement('div');
+    
+    var dataArr[];
+    var softArr[]; 
+    var webArr[];
+    
+    var people = "http://ict.neit.edu/evanrense/salaries.php";
+    
+    fetch(people)
+    .then (function())
+    {
+           
+    }
+}
