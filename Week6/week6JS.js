b030d6c407c6ecc3223b6b2d289344ce20c8ddc8
Trumpold <bltrumpold@labs.neit.edu>
2018-08-21 13:48:25 -0400

    lab 6

--- Week6/week6JS.js
+++ Week6/week6JS.js
@@ -6,15 +6,27 @@ function doit()
     var webDev = document.createElement('div');
     var softDev = document.createElement('div');
     
-    var dataArr[];
-    var softArr[]; 
-    var webArr[];
+    var fName = document.querySelector("#first");
+    
+    var lName = document.querySelector("#last");
+    
+    var highPaid = document.querySelector("#highest");
+    
+    var highAvgPaid = document.querySelector("#average")
+    
+    //var dataArr[];
+    //var softArr[]; 
+    //var webArr[];
     
     var people = "http://ict.neit.edu/evanrense/salaries.php";
     
     fetch(people)
-    .then (function())
+    .then(function(response)
     {
-           
-    }
+        return response.json();
+    })
+    .then(function(myJson)
+    {
+        console.log(myJson);
+    });
 }
